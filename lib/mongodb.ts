eace9f2576b94e76444855c56bd32ab760be5961
severinnhq <sstierbusiness@gmail.com>
2024-11-24 00:23:41 +0100

    ready

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,15 +5,20 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI
-const options = {}
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  ssl: true,
+  sslValidate: true,
+}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  const globalWithMongo = global as typeof globalThis & {
+  let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>
   }
 
