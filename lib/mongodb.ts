a4e073728c7e16cc468a4ac667a1d14667d4a200
severinnhq <sstierbusiness@gmail.com>
2024-11-23 22:29:33 +0100

    ready

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -11,7 +11,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI
 const options = {}
 
-let client
+let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === "development") {
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "development") {
     client = new MongoClient(uri, options)
     global._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = global._mongoClientPromise as Promise<MongoClient>
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
