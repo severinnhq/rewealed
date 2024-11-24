8b849e104f894e5423cc89c83a869f914bcf8c0b
severinnhq <sstierbusiness@gmail.com>
2024-11-24 18:19:20 +0100

    ready

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -48,7 +48,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     if (chunkIndex === totalChunks - 1) {
-      // All  chunks received, file is complete
+      // All chunks received, file is complete
       res.status(200).json({ message: 'File uploaded successfully' });
     } else {
       res.status(200).json({ message: 'Chunk received' });
