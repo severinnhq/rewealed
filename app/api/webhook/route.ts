ac130e7c5c46932e71f898fbf19e3b18647fa2df
severinnhq <sstierbusiness@gmail.com>
2024-11-28 19:10:52 +0100

    ready

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 import clientPromise from '@/lib/mongodb'
-import { ObjectId } from 'mongodb'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-11-20.acacia',
